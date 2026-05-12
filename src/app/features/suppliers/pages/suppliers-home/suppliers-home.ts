052fe529ed45f2028b461542426cd8d49185dfee
duKraut <edukrautchuk@gmail.com>
2026-05-12 13:21:46 -0300

    feature: suppliers home & css

--- src/app/features/suppliers/pages/suppliers-home/suppliers-home.ts
+++ src/app/features/suppliers/pages/suppliers-home/suppliers-home.ts
@@ -7,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './suppliers-home.css',
 })
 export class SuppliersHome {
+  allActives = [
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+  ];
 
+    paginaAtual = 1;
+  itensPorPagina = 5;
+
+  get totalPaginas() { return Math.ceil(this.allActives.length / this.itensPorPagina); }
+  
+  get itemsDisplayed() {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    return this.allActives.slice(inicio, inicio + this.itensPorPagina);
+  }
+
+  get arrayPaginas() {
+    return Array(this.totalPaginas).fill(0).map((x, i) => i + 1);
+  }
+  irParaPagina(pagina: number) {
+    this.paginaAtual = pagina;
+  }
+  proximaPagina() {
+    if (this.paginaAtual < this.totalPaginas) this.paginaAtual++;
+  }
+  paginaAnterior() {
+    if (this.paginaAtual > 1) this.paginaAtual--;
+  }  
 }
