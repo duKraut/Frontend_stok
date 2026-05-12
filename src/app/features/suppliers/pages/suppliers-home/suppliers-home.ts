4f00b4cdee35e88d9b88c24df27e46bb9dbbb29c
duKraut <edukrautchuk@gmail.com>
2026-05-12 19:51:34 -0300

    feat ajustes suppliers home

--- src/app/features/suppliers/pages/suppliers-home/suppliers-home.ts
+++ src/app/features/suppliers/pages/suppliers-home/suppliers-home.ts
@@ -8,19 +8,19 @@ import { Component } from '@angular/core';
 })
 export class SuppliersHome {
   allActives = [
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
-    {name: 'teste', cnpj: '11.111.111/1111-11', agent: 'pessoa teste', cpf: '111.111.111-11', email: 'mail@mail.com'},
+    {name: 'teste', cnpj_cpf: '111.111.111-11', person_contact: 'teste', email: 'mail@mail.com', type: 'Física', stats: 'Ativo'},
+    {name: 'teste', cnpj_cpf: '222.222.222-22', person_contact: 'teste', email: 'mail@mail.com', type: 'Física', stats: 'Ativo'},
+    {name: 'teste', cnpj_cpf: '33.333.333/3333-33', person_contact: 'teste', email: 'mail@mail.com', type: 'Jurídica', stats: 'Ativo'},
+    {name: 'teste', cnpj_cpf: '444.444.444-44', person_contact: 'teste', email: 'mail@mail.com', type: 'Física', stats: 'Inativo'},
+    {name: 'teste', cnpj_cpf: '555.555.555-55', person_contact: 'teste', email: 'mail@mail.com', type: 'Física', stats: 'Inativo'},
+    {name: 'teste', cnpj_cpf: '666.666.666-66', person_contact: 'teste', email: 'mail@mail.com', type: 'Jurídica', stats: 'Inativo'},
+    {name: 'teste', cnpj_cpf: '77.777.777/7777-77', person_contact: 'teste', email: 'mail@mail.com', type: 'Jurídica', stats: 'Ativo'},
+    {name: 'teste', cnpj_cpf: '888.888.888-88', person_contact: 'teste', email: 'mail@mail.com', type: 'Física', stats: 'Inativo'},
+    {name: 'teste', cnpj_cpf: '99.999.999/9999-99', person_contact: 'teste', email: 'mail@mail.com', type: 'Jurídica', stats: 'Ativo'},
   ];
 
     paginaAtual = 1;
-  itensPorPagina = 5;
+  itensPorPagina = 6;
 
   get totalPaginas() { return Math.ceil(this.allActives.length / this.itensPorPagina); }
   
