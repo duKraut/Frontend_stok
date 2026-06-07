2d282525a6d28f3db4d237701ea19a557bd0f7c8
duKraut <edukrautchuk@gmail.com>
2026-06-07 00:50:39 -0300

    feature: requisições carregando na home comfiltros funcionais

--- src/app/features/assets/pages/assets-home/assets-home.ts
+++ src/app/features/assets/pages/assets-home/assets-home.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderService } from '../../../../core/services/header';
+import { Asset, AssetService } from '../../services/asset.servce';
 
 type BemFormMode = 'create' | 'edit' | 'view';
 
@@ -11,14 +12,7 @@ type BemFormMode = 'create' | 'edit' | 'view';
   styleUrl: './assets-home.css',
 })
 export class AssetsHome implements OnInit {
-  todosAtivos = [
-    { tombamento: '1', nome: 'Notebook Dell Latitude 5420', desc: 'Core i7, 16GB RAM, 512GB SSD', marca: 'Dell', categoria: 'Informática', aquisicao: '12/05/2022', depto: 'TI - Sede', estado: 'EXCELENTE', estadoClass: 'badge-excelente', valor: 'R$ 5.400,00' },
-    { tombamento: '2', nome: 'Ar Condicionado 12k BTU', desc: 'Samsung Digital Inverter', marca: 'Samsung', categoria: 'Equipamentos de Escritório', alerta: true, aquisicao: '15/03/2019', depto: 'Vendas', estado: 'MANUTENÇÃO', estadoClass: 'badge-manutencao', valor: 'R$ 2.100,00' },
-    { tombamento: '3', nome: 'Cadeira Ergonômica Pro', desc: 'Ajuste de Lombar e Braços', marca: 'GenioDesk', categoria: 'Mobiliário', aquisicao: '20/01/2021', depto: 'Financeiro', estado: 'BOM', estadoClass: 'badge-bom', valor: 'R$ 1.200,00' },
-    { tombamento: '4', nome: 'Projetor Epson X41', desc: 'Sala de Reunião A', marca: 'Epson', categoria: 'Eletrônicos', perigo: true, aquisicao: '05/06/2018', depto: 'Adm.', estado: 'SUBSTITUIR', estadoClass: 'badge-substituir', valor: 'R$ 800,00' },
-    { tombamento: '5', nome: 'Servidor PowerEdge R740', desc: 'Rack 01 - Data Center', marca: 'Dell', categoria: 'Informática', aquisicao: '10/11/2023', depto: 'TI - Infra', estado: 'EXCELENTE', estadoClass: 'badge-excelente', valor: 'R$ 45.000,00' },
-    { tombamento: '6', nome: 'Monitor Dell 24"', desc: 'IPS Full HD', marca: 'Dell', categoria: 'Informática', aquisicao: '12/11/2023', depto: 'TI - Infra', estado: 'EXCELENTE', estadoClass: 'badge-excelente', valor: 'R$ 1.000,00' }
-  ];
+  allAssets: Asset[] = [];
 
   activeTab: 'Todos' | 'Excelente' | 'Bom' | 'Manutenção' | 'Substituir' = 'Todos';
   paginaAtual = 1;
@@ -29,16 +23,16 @@ export class AssetsHome implements OnInit {
   selectedBem: any = null;
 
   get ativosFiltrados() {
-    if (this.activeTab === 'Excelente')  return this.todosAtivos.filter(a => a.estado === 'EXCELENTE');
-    if (this.activeTab === 'Bom')        return this.todosAtivos.filter(a => a.estado === 'BOM');
-    if (this.activeTab === 'Manutenção') return this.todosAtivos.filter(a => a.estado === 'MANUTENÇÃO');
-    if (this.activeTab === 'Substituir') return this.todosAtivos.filter(a => a.estado === 'SUBSTITUIR');
-    return this.todosAtivos;
+    if (this.activeTab === 'Excelente')  return this.allAssets.filter(a => a.conservationStatus === 'EXCELENTE');
+    if (this.activeTab === 'Bom')        return this.allAssets.filter(a => a.conservationStatus === 'BOM');
+    if (this.activeTab === 'Manutenção') return this.allAssets.filter(a => a.conservationStatus === 'MANUTENCAO');
+    if (this.activeTab === 'Substituir') return this.allAssets.filter(a => a.conservationStatus === 'SUBSTITUIR');
+    return this.allAssets;
   }
 
   get totalPaginas() { return Math.ceil(this.ativosFiltrados.length / this.itensPorPagina); }
 
-  get itensExibidos() {
+  get itemsDisplayed() {
     const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
     return this.ativosFiltrados.slice(inicio, inicio + this.itensPorPagina);
   }
@@ -78,7 +72,7 @@ export class AssetsHome implements OnInit {
     this.router.navigate(['/assets/movements']);
   }
 
-  constructor(private headerService: HeaderService, private router: Router) {}
+  constructor(private headerService: HeaderService, private router: Router, private assetService: AssetService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.headerService.setConfig({
@@ -86,6 +80,18 @@ export class AssetsHome implements OnInit {
       showSearch: true,
       primaryButtonLabel: 'Novo Bem',
       primaryButtonIcon: 'ph ph-plus'
+    });
+
+    this.loadAssets();
+  }
+
+  loadAssets(): void {
+    this.assetService.getAll().subscribe({
+      next: (data) => {
+        this.allAssets = data;
+        this.cdr.detectChanges();
+      },
+      error: (error) => console.error('Erro ao buscar bens', error)
     });
   }
 }
