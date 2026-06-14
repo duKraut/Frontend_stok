98cfa8f509f0d29ca10cfba24704884dee44d0fd
duKraut <edukrautchuk@gmail.com>
2026-06-14 04:08:20 -0300

    fix: correção de valor patrimoniado e locale dos valores

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,11 +1,15 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { CoreModule } from './core/core-module';
 import { PrivateLayout } from './layouts/private-layout/private-layout';
 import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -20,6 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [App]
 })
